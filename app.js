2c8fb7b2a47c620e663460e2bb964acd24922f92
warkaysofyan <mrwarkay@gmail.com>
2022-12-27 21:19:53 +0000

    second commition

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const express = require("express");
-const path = require("path");
-const morgan = require("morgan");
-const { dirname } = require("path");
+//include core modules
+const path = require('path');
 
+//include node modules
+const express = require('express');
+
+//include npm modules
+const morgan = require('morgan');
+
+//including router modules
+
+const users = require('./routes/users');
+
+//initializing the express app  and assigning the port variable
 port = process.env.PORT || 9000;
 const app = express();
-app.set("view engine", "ejs");
-app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/home", (req, res, next) => {
-  res.render("index");
-});
+// setting the view engine
+app.set('view engine', 'ejs');
+
+//meddlewares
+app.use(morgan('dev'));
+app.use('/home', users);
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // starting the server ;
 app.listen(port, () => console.log(`app is starting on port ${port}`));
